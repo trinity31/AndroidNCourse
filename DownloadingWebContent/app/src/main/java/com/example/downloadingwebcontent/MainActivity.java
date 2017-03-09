b6fb675072486d741e60a74c34d6188fc3c63f47
Trinity Song <kyeongyeon.song@gmail.com>
2017-03-09 13:11:56 +0900

    Downloading Web Content and Images

---
+++ DownloadingWebContent/app/src/main/java/com/example/downloadingwebcontent/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.downloadingwebcontent;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return "Done";
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+        try {
+            result = task.execute("https://www.ecowebhosting.co.uk/").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("Result", result);
+    }
+}
